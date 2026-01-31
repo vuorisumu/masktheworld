3f5fe7b78299208a281cc2202ee1ba36e845fd86
Sumu Vuori <sumu.vuori@tuni.fi>
2026-01-31 19:18:56 +0200

    add stage maps

--- src/components/views/GameScreen.tsx
+++ src/components/views/GameScreen.tsx
@@ -3,13 +3,17 @@ import Test from "../../pateScene/Test";
 import MaskButtons from "../../sumuScene/MaskButtons";
 import { useAppContext } from "../../utils/AppContext";
 import GameContext from "../../utils/GameContext";
-import type { Block, MaskType } from "../../utils/types";
+import { type Block, type MapType, type MaskType } from "../../utils/types";
 import ButtonPrompt from "../ButtonPrompt";
 
 export default function GameScreen() {
   const [mask, setMask] = useState<MaskType>("normal");
   const [stage, setStage] = useState(0);
   const { changeScene, allMaps } = useAppContext();
+
+  const currentStageMaps = useMemo<MapType[]>(() => {
+    return allMaps.filter((m) => m.stage === stage);
+  }, [stage, allMaps]);
 
   const currentMap = useMemo<Block[][]>(() => {
     const match = allMaps.find((m) => m.stage === stage && m.mask === mask);
@@ -40,15 +44,11 @@ export default function GameScreen() {
 
   return (
     <GameContext.Provider
-      value={{ mask, setMask, stage, stageUp, stageDown, currentMap, getBlock }}
+      value={{ mask, setMask, stage, stageUp, stageDown, currentMap, currentStageMaps, getBlock }}
     >
       <MaskButtons />
       <Test />
-      <ButtonPrompt
-        buttonText='Luovuta'
-        onConfirm={quitGame}
-        promptText='U SURE??'
-      />
+      <ButtonPrompt buttonText="Luovuta" onConfirm={quitGame} promptText="U SURE??" />
     </GameContext.Provider>
   );
 }
