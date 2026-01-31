2c7bda865e9919e7c169ef5c313d4ebd7fda97d3
Sumu Vuori <sumu.vuori@tuni.fi>
2026-01-31 16:31:57 +0200

    fix map issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ScreenHandler from "./sumuScene/ScreenHandler";
 import AppContext from "./utils/AppContext";
-import { type ScreenName } from "./utils/types";
+import { type Block, type MapType, type ScreenName } from "./utils/types";
 
 function App() {
   const [screen, setScreen] = useState<ScreenName>("start");
+  const [allMaps, setAllMaps] = useState<MapType[]>([]);
+
+  useEffect(() => {
+    const maps: MapType[] = [
+      { stage: 0, mask: "normal", file: "stage0base" },
+      { stage: 0, mask: "other", file: "stage0masked" },
+      { stage: 1, mask: "normal", file: "mapTest" },
+      { stage: 1, mask: "other", file: "maskedLevel" },
+    ];
+
+    const loadMaps = async () => {
+      const loaded = await Promise.all(
+        maps.map(async (map) => {
+          const r = await fetch(`${map.file}.txt`);
+          const txt = await r.text();
+          const level: Block[][] = txt
+            .trim()
+            .split("\n")
+            .map((row) =>
+              row
+                .split("")
+                .filter((v) => v !== "\r")
+                .map((col) => {
+                  console.log(col);
+                  switch (col) {
+                    case "X":
+                      return { solid: true, name: "wall" };
+                    case "-":
+                      return { solid: false, name: "floor" };
+                    default:
+                      return { solid: false, name: "floor" };
+                  }
+                }),
+            );
+
+          return { ...map, level };
+        }),
+      );
+
+      setAllMaps(loaded);
+    };
+
+    loadMaps();
+  }, []);
 
   const changeScene = (newScreen: ScreenName) => {
     // virheidenhallinta tänne
@@ -13,7 +57,7 @@ function App() {
   };
 
   return (
-    <AppContext.Provider value={{ screen, changeScene }}>
+    <AppContext.Provider value={{ screen, changeScene, allMaps }}>
       <ScreenHandler />
     </AppContext.Provider>
   );
