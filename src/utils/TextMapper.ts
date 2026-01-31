49fa0694c6a576e5967a70bd7837f3921a178479
Sumu Vuori <sumu.vuori@tuni.fi>
2026-01-31 14:32:57 +0200

    fix mapper

--- src/utils/TextMapper.ts
+++ src/utils/TextMapper.ts
@@ -1,43 +1,56 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useGameContext } from "./GameContext";
 import type { Block, MapType } from "./types";
 
 export function useGetMap(): Block[][] {
   const { mask, stage } = useGameContext();
-  const allMaps = useMemo<MapType[]>(() => {
-    return [
+  const [allMaps, setAllMaps] = useState<MapType[]>([]);
+
+  useEffect(() => {
+    const maps: MapType[] = [
       { stage: 0, mask: "normal", file: "stage0base" },
       { stage: 0, mask: "other", file: "stage0masked" },
       { stage: 1, mask: "normal", file: "mapTest" },
       { stage: 1, mask: "other", file: "maskedLevel" },
     ];
-  }, []);
 
-  const maps = useMemo<MapType[]>(() => {
-    const mapArr: MapType[] = [];
-    allMaps.forEach(async (obj) => {
-      const r = await fetch(`${obj.file}.txt`);
-      const txt = await r.text();
-      const processed = txt.split("\n").map((r) => r.split(""));
-      const level = processed.map((row) =>
-        row.map((col) => {
-          switch (col) {
-            case "X":
-              return { solid: true, name: "wall" };
-            case "-":
-              return { solid: false, name: "floor" };
-            default:
-              return { solid: false, name: "floor" };
-          }
+    const loadMaps = async () => {
+      const loaded = await Promise.all(
+        maps.map(async (map) => {
+          const r = await fetch(`${map.file}.txt`);
+          const txt = await r.text();
+          const level: Block[][] = txt
+            .trim()
+            .split("\n")
+            .map((row) =>
+              row
+                .split("")
+                .filter((v) => v !== "\r")
+                .map((col) => {
+                  console.log(col);
+                  switch (col) {
+                    case "X":
+                      return { solid: true, name: "wall" };
+                    case "-":
+                      return { solid: false, name: "floor" };
+                    default:
+                      return { solid: false, name: "floor" };
+                  }
+                }),
+            );
+
+          return { ...map, level };
         }),
       );
-      mapArr.push({ ...obj, level });
-    });
-    return mapArr;
-  }, [allMaps]);
+
+      setAllMaps(loaded);
+    };
+
+    loadMaps();
+  }, []);
 
   return useMemo<Block[][]>(() => {
-    const match = maps.find((m) => m.stage === stage && m.mask === mask);
+    const match = allMaps.find((m) => m.stage === stage && m.mask === mask);
     if (match && match.level) {
       console.log("Currently used level has changed", match);
       return match.level;
@@ -45,5 +58,5 @@ export function useGetMap(): Block[][] {
       console.log("No map associated with stage", stage, "and mask", mask);
     }
     return [[]];
-  }, [stage, mask, maps]);
+  }, [stage, mask, allMaps]);
 }
